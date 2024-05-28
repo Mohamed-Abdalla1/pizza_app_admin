59d7c709b227d4a237937c87c1e01e734011753e
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-05-28 11:01:45 +0300

    upload image states done

--- lib/screens/create/blocs/bloc/upload_image_state.dart
+++ lib/screens/create/blocs/bloc/upload_image_state.dart
@@ -2,9 +2,19 @@ part of 'upload_image_bloc.dart';
 
 sealed class UploadImageState extends Equatable {
   const UploadImageState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class UploadImageInitial extends UploadImageState {}
+
+final class UploadImageLoading extends UploadImageState {}
+
+final class UploadImageSuccess extends UploadImageState {
+  final String url;
+
+  const UploadImageSuccess({required this.url});
+}
+
+final class UploadImageFailure extends UploadImageState {}
