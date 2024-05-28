e2608b202bc53e9e036488423b883617080025d6
Mohamed-Abdalla1 <mohamedfathalla2772@gmail.com>
2024-05-28 13:20:29 +0300

    create pizza states done

--- lib/screens/create/blocs/create_pizza/create_pizza_state.dart
+++ lib/screens/create/blocs/create_pizza/create_pizza_state.dart
@@ -2,9 +2,19 @@ part of 'create_pizza_bloc.dart';
 
 sealed class CreatePizzaState extends Equatable {
   const CreatePizzaState();
-  
+
   @override
   List<Object> get props => [];
 }
 
 final class CreatePizzaInitial extends CreatePizzaState {}
+
+final class CreatePizzaLoading extends CreatePizzaState {}
+
+final class CreatePizzaSuccess extends CreatePizzaState {
+  final PizzaModel pizzaModel;
+
+ const CreatePizzaSuccess({required this.pizzaModel});
+}
+
+final class CreatePizzaFailure extends CreatePizzaState {}
